34352741660b339be439a5c9b1f7f21b4245bd1d
Dell <Dell@DESKTOP-O64CFS8>
2024-03-11 16:25:19 +0000

    First commit

---
+++ HelloWorld_Maven/src/test/java/HelloWorld_Maven/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package HelloWorld_Maven;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testhelloworld() {
+		
+		System.out.println("hello worldsdfedsfsfsdfsdf");
+	}
+	
+}
